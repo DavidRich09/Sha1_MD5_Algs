7973e7e8495360dcefbfee4e28d4653ed7611afe
Tiquillo <lujimenez2000@gmail.com>
2021-06-11 18:58:06 -0600

    más correcciones de desencriptación

--- Files.cpp
+++ Files.cpp
@@ -1,23 +1,10 @@
-#include <string>
+//
+// Created by lujim on 11/6/2021.
+//
+
 #include "Files.h"
 
-
-bool Files::ContainsChar(const std::string& fragment, const char& character, int &position) {
-    /* Checks if a string contains a character and assigns the position by reference
-     */
-
-    for (int i = 0; i < fragment.length(); i++) {
-        if (fragment.at(i) == character) {
-            position = i;
-            return true;
-        }
-    }
-    position = NOT_STRING_POS_OR_LENGHT;
-    return false;
-}
-
-bool Files::ContainsStr(const std::string &text, const std::string& fragment, int &position, int &lenght) {
-
+bool Files::ContainsStr(const std::string &text, const std::string &fragment, int &position, int &lenght) {
     int fragIter = 0;                        // initializes fragment iterator
     lenght = fragment.length();             // asigns length to fragment lenght
     for (int i = 0; i < text.length(); i++){
@@ -37,7 +24,7 @@ bool Files::ContainsStr(const std::string &text, const std::string& fragment, in
     return false;
 }
 
-std::string Files::SearchSHA1Hash(std::string hash) {
+std::string Files::SearchSHA1Hash(const std::string &hash) {
     int position = NOT_STRING_POS_OR_LENGHT;
     int length = NOT_STRING_POS_OR_LENGHT;
 
@@ -47,17 +34,15 @@ std::string Files::SearchSHA1Hash(std::string hash) {
         while (std::getline(file, line)) {
             if (ContainsStr(line, hash, position, length)) {
                 file.close();
-                return line;
+                return line.substr(0,position);
             }
         }
     } else {} // couldn't open file
 
     return std::string();
-
 }
 
 void Files::AddSHA1Hash(const std::string &word, const std::string &hash) {
-
     std::ifstream rfile ("../sha1.txt");
     std::string ftext;
 
@@ -78,17 +63,17 @@ void Files::AddSHA1Hash(const std::string &word, const std::string &hash) {
     } else {} // couldn't open file
 }
 
-std::string Files::SearchMD5Hash(std::string hash) {
+std::string Files::SearchMD5Hash(const std::string& hash) {
     int position = NOT_STRING_POS_OR_LENGHT;
     int length = NOT_STRING_POS_OR_LENGHT;
 
     std::string line;
-    std::ifstream file("../md5.txt");
+    std::ifstream file ("../md5.txt");
     if (file.is_open()) {
         while (std::getline(file, line)) {
             if (ContainsStr(line, hash, position, length)) {
                 file.close();
-                return line;
+                return line.substr(0,position);
             }
         }
     } else {} // couldn't open file
--- Files.h
+++ Files.h
@@ -1,17 +1,22 @@
+#ifndef EXTRACLASE4_FILES_H
+#define EXTRACLASE4_FILES_H
+
 #include <fstream>
+#include <string>
 
 #define NOT_STRING_POS_OR_LENGHT (-00100000)
 
 class Files {
 private:
-    static bool ContainsChar(const std::string& fragment, const char& character, int &position);
-
     static bool ContainsStr(const std::string &text, const std::string& fragment, int &position, int &lenght);
 
 public:
-    static std::string SearchSHA1Hash(std::string hash);
+    static std::string SearchSHA1Hash(const std::string& hash);
     static void AddSHA1Hash(const std::string& word, const std::string& hash);
 
-    static std::string SearchMD5Hash(std::string hash);
+    static std::string SearchMD5Hash(const std::string& hash);
     static void AddMD5Hash(const std::string& word, const std::string& hash);
 };
+
+
+#endif //EXTRACLASE4_FILES_H
